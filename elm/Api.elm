iteloo/the-island

commit 0b003c760d7666da6b8784b866d2a53a1f9e5543

Minor cleanup (#15)

elm/Api.elm
@@ -89,20 +89,7 @@ actionHelp a =
 
         "trade_completed" ->
             D.map TradeCompleted <|
-                D.field "materials"
-                    (D.string
-                        |> D.andThen
-                            (D.decodeString (material D.int)
-                                >> (\r ->
-                                        case r of
-                                            Ok mat ->
-                                                D.succeed mat
-
-                                            Err e ->
-                                                D.fail e
-                                   )
-                            )
-                    )
+                D.field "materials" (material D.int)
 
         "event" ->
             -- [todo] add better logic

commit d942df94696bf5939cb107eac8edf42e57fd0be3

Add trade button; Fixes trade completed bug caused by #15

elm/Api.elm
@@ -89,8 +89,19 @@ actionHelp a =
                 )
 
         "trade_completed" ->
+            -- [note] material field is whatever str we passed in to `trade` msg
             D.map TradeCompleted <|
-                D.field "materials" (material D.int)
+                (D.field "materials" D.string
+                    |> D.andThen
+                        (\materialStr ->
+                            case D.decodeString (material D.int) materialStr of
+                                Ok resourcesObtained ->
+                                    D.succeed resourcesObtained
+
+                                Err e ->
+                                    D.fail e
+                        )
+                )
 
         "event" ->
             -- [todo] add better logic
@@ -225,7 +236,9 @@ encodeServerAction a =
                 Trade mat ->
                     ( "trade"
                     , [ ( "materials"
-                        , E.string
+                        , -- [note] in `trade_completed`, we'll get back
+                          -- string we pass in here
+                          E.string
                             (E.encode 0 (encodeMaterial E.int mat))
                         )
                       ]

elm/Msg.elm
@@ -43,6 +43,7 @@ type GameMsg
     | MoveToBasket Resource Int
     | EmptyBasket
     | Shake
+    | TradeButton
 
 
 type SiteSelectionMsg

elm/Update.elm
@@ -101,8 +101,7 @@ updateApp ctx msg model =
         ServerMsgReceived action ->
             case action of
                 Ok action ->
-                    model
-                        |> handleAction action
+                    model |> handleAction action
 
                 Err e ->
                     Debug.crash <| "Error in ServerMsgReceived: " ++ e
@@ -133,6 +132,9 @@ updateGame { toServer, toMsg } msg model =
     let
         toGameServer =
             toServer model.gameName
+
+        trade model =
+            model ! [ toGameServer (Api.Trade model.basket) ]
     in
     case msg of
         WaitMsg msg ->
@@ -195,7 +197,10 @@ updateGame { toServer, toMsg } msg model =
                 ! []
 
         Shake ->
-            model ! [ toGameServer (Api.Trade model.basket) ]
+            trade model
+
+        TradeButton ->
+            trade model
 
 
 updateSiteSelection :

elm/View.elm
@@ -212,6 +212,8 @@ basketView { inventory, basket } =
                             , resourceView -1 basket
                             , [ button [ onClick EmptyBasket ]
                                     [ text "Empty" ]
+                              , button [ onClick TradeButton ]
+                                    [ text "Trade" ]
                               ]
                             ]
               , row [] <|